8f221625c1b440be7c0147871394c9aaa38dd4cf
3bd-allah <abdullah.nabil5044@gmail.com>
2024-04-02 12:49:09 +0200

    setting up style v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import SEO from './components/SEO';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import {NavComponent} from './components/Nav';
 
 function App() {
   return (
     <>
-      <SEO />
+      <SEO/>
+      <NavComponent/>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
+
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,44 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-const Nav = () => {
+export const NavComponent = () => {
   return (
     <nav>
-      <div>Nav</div>
+      <Navbar bg="" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="">
+            <img src={require("../assets/logo L.jpg")} className="w-25" />
+          </Navbar.Brand>
+          <Nav className="me-auto links d-flex">
+            <Nav.Link href="#home" className="text-black">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#features" className="text-black">
+              About
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="text-black">
+              Menu
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="text-black">
+              Reservations
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="text-black">
+              Order Online
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="text-black">
+              Login
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <nav
+        class="navbar bg-dark border-bottom border-body text-white"
+        data-bs-theme="dark"
+      >
+        Little Lemon
+      </nav>
     </nav>
   );
 };
